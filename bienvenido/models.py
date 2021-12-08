fbb951e5b46fd276daadb76d5d7f51cd9663fe2c
BrhmDnsK263 <denysiuk.isi@gmail.com>
2021-12-08 20:07:57 -0300

    cambios modelo

--- bienvenido/models.py
+++ bienvenido/models.py
@@ -7,8 +7,10 @@ class Departamento(models.Model):
     fecha_modificacion = models.DateTimeField(auto_now=True)
 
 class Empleado(models.Model):
-    nombre = models.CharField(max_length=50, null=False, blank=False)
-    apellido = models.CharField(max_length=50, null=False, blank=False)
-    fecha_nacimiento = models.DateTimeField()
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    apellido = models.CharField(max_length=100, null=False, blank=False)
+    fecha_nacimiento = models.DateTimeField(default=None)
     sueldo = models.DecimalField(max_digits=10, decimal_places=2, default=10000)
     departamento = models.ForeignKey(Departamento, on_delete=models.SET_NULL, null=True)
+    dni = models.CharField(max_length=8, unique=True)
+    apodo =  models.CharField(max_length=100, null=False, blank=False, default="sin apodo")
