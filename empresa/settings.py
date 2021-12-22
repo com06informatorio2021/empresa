97a2adfbffb6621fe75c4b662ca7afc94975905c
BrhmDnsK263 <denysiuk.isi@gmail.com>
2021-12-22 20:51:27 -0300

    debug false

--- empresa/settings.py
+++ empresa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-j_$*gr0s^j3!*ela!5+ws
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
